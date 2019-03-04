c3737c333ce62f8c06ae9df8cfb3654da67d55ae
Stevens LAURENT <sio.laurent.stevens@gmail.com>
2019-03-04 16:36:10 +0100

    commit prod version

---
+++ app.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.sendfile('./test.html');
+});
+
+
+app.get('/camera', function(req, res){
+
+  var path = require('path');
+  //console.log(path.resolve(__dirname+'/../three.js/examples/basic.html'));
+res.sendFile(path.resolve(__dirname+'/three.js/examples/basic.html'));
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+})
