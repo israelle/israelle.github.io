2f6cca4d13f07681afb964ec36467428c77dbefe
israelle <israelle.lagier972@gmail.com>
2019-03-04 15:59:26 +0100

    ILR => modify button

--- project/app.js
+++ project/app.js
@@ -5,6 +5,13 @@ app.get('/', function(req, res){
   res.sendfile('./test.html');
 });
 
+app.get('/camera', function(req, res){
+
+  var path = require('path');
+  console.log(path.resolve(__dirname));
+res.sendFile(path.resolve(__dirname+'/../three.js/examples/basic.html'));
+
+});
 http.listen(3000, function(){
   console.log('listening on *:3000');
 })
